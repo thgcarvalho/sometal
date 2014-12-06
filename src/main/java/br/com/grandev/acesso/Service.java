cad291a6d4728e06d4edfcb9b473119caaac4f4b
tcarvalho <tcarvalho@grandev.com.br>
2014-12-05 22:19:00 -0300

    Novo Servidor

--- src/main/java/br/com/grandev/acesso/Service.java
+++ src/main/java/br/com/grandev/acesso/Service.java
@@ -1,6 +1,6 @@
 package br.com.grandev.acesso;
 
 public class Service {
-    public static final String IP = "192.168.2.5";
+    public static final String IP = "184.107.22.174";
     public static  int PORT = 7788;
 }
